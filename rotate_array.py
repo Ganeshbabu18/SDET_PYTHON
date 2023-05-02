a8ec7219368f06f9e353f18fc6bd5dd78318bfc8
Ganesh babu <95689460+Ganeshbabu18@users.noreply.github.com>
2023-05-02 16:51:59 +0530

    Update rotate_array.py

--- rotate_array.py
+++ rotate_array.py
@@ -1,17 +1,17 @@
-def rotate_array(arr, k):
+def rotate_array(arr, rotations_array):
 
-    n = len(arr)
-    k = k % n
-    arr[:n - k] = arr[:n - k][::-1]
-    arr[n - k:] = arr[n - k:][::-1]
+    length_array = len(arr)
+    rotations_array = rotations_array % length_array
+    arr[:length_array- rotations_array] = arr[:length_array - rotations_array][::-1]
+    arr[length_array - rotations_array:] = arr[length_array - rotations_array:][::-1]
     arr[:] = arr[::-1]
 
     return arr
 
 arr = [1, 2, 3, 4, 5]
-k = 2
+rotations_array = 2
 
-rotated_arr = rotate_array(arr, k)
+rotated_arr = rotate_array(arr, rotations_array)
 
 print(rotated_arr)
 

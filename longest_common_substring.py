ec08cfacd367085ca775758e98cc9f6ebf6ee812
Ganesh babu <95689460+Ganeshbabu18@users.noreply.github.com>
2023-05-03 00:10:53 +0530

    Update longest_common_substring.py

--- longest_common_substring.py
+++ longest_common_substring.py
@@ -1,15 +1,15 @@
-def longest_common_substring(X: str, Y: str) -> int:
-    m, n = len(X), len(Y)
-    table = [[0] * (n+1) for _ in range(m+1)]
+def longest_common_substring(str_one: str, str_two: str) -> int:
+    len_one, len_two = len(str_one), len(str_two)
+    table = [[0] * (len_one + 1) for _ in range(len_two + 1)]
     max_len = 0
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            if X[i-1] == Y[j-1]:
-                table[i][j] = table[i-1][j-1] + 1
+    for i in range(1, len_one + 1):
+        for j in range(1, len_two + 1):
+            if str_one[i - 1] == str_two[j - 1]:
+                table[i][j] = table[i - 1][j - 1] + 1
                 max_len = max(max_len, table[i][j])
     return max_len
 
 
-X = "abcdxyz"
-Y = "xyzabcd"
-print(longest_common_substring(X, Y))
+str_one = "abcdxyz"
+str_two = "xyzabcd"
+print(longest_common_substring(str_one, str_two))

6c422cb9a6d11c1bbe6d3b42408c64bd116877ee
Ganesh Babu <ganeshb@synerzipune.local>
2023-06-06 11:51:44 +0530

    ten new

---
+++ Equal_part_strings.py
@@ -0,0 +1,21 @@
+# Q: Python program to divide a string in 2 equal parts. Input string = "Ganeshbabu"
+
+def divide_string(string, n):
+    length = len(string)
+    if length % n != 0:
+        print("Cannot divide the string into equal parts.")
+        return []
+
+    part_size = length // n
+    parts = [string[i:i + part_size] for i in range(0, length, part_size)]
+    return parts
+
+
+# Input
+string_to_divide = "Ganeshbabu"
+number_of_parts = 2
+
+result = divide_string(string_to_divide, number_of_parts)
+print(result)
+
+# Output: ['Ganes', 'hbabu']
